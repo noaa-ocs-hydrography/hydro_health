08f89f62dd6b819904326531521b8524b0011f2f
stephen.patterson <stephen.patterson@noaa.gov>
2024-04-26 12:59:58 -0400

    finalized rasterize reef layers

--- scripts/download_hydrohealth_data.py
+++ scripts/download_hydrohealth_data.py
@@ -8,7 +8,6 @@ from hydro_health.helpers.tools import get_config_item
 
 
 INPUTS = pathlib.Path(__file__).parents[1] / 'inputs'
-OUTPUTS = pathlib.Path(__file__).parents[1] / 'outputs'
 
 
 def get_reef_5k_json(save: bool=False) -> dict:
@@ -32,7 +31,7 @@ def get_reef_5k_json(save: bool=False) -> dict:
                 "type": "FeatureCollection", 
                 "features": value
             }
-            with open(OUTPUTS / f'global_reef_{key}.geojson', 'w') as features:
+            with open(INPUTS / f'global_reef_{key}.geojson', 'w') as features:
                 features.write(json.dumps(template))
 
     return polygons
@@ -41,12 +40,12 @@ def get_reef_5k_json(save: bool=False) -> dict:
 def convert_json_to_shp():
     """Convert Reef 5k polygons to shapefile using Geopandas"""
 
-    reef_polygons_json = str(OUTPUTS / 'global_reef_polygons.geojson')
+    reef_polygons_json = str(INPUTS / 'global_reef_polygons.geojson')
     print(reef_polygons_json)
     if not os.path.exists(reef_polygons_json):
         get_reef_5k_json(save=True)
     gdf = geopandas.read_file(reef_polygons_json)
-    global_reef_polygons = str(OUTPUTS / 'global_reef_polygons_5k.shp')
+    global_reef_polygons = str(INPUTS / 'global_reef_polygons_5k.shp')
     gdf.to_file(global_reef_polygons)
     return global_reef_polygons
 
@@ -55,8 +54,8 @@ def get_reef_1km_data() -> dict:
     """Download global reef-1km features"""
 
     url = get_config_item('REEF', '1KM')
-    zip_file = OUTPUTS / 'global_reefs_1km.zip'
-    zip_folder = OUTPUTS / 'global_reefs_1km'
+    zip_file = INPUTS / 'global_reefs_1km.zip'
+    zip_folder = INPUTS / 'global_reefs_1km'
     global_reefs_download = requests.get(url, stream=True)
     with open(zip_file, 'wb') as data:
         print('Saving 1km Reef zip file')
