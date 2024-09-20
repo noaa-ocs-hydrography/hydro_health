2ebb90658290886e0d7799549d3cdcf44b274ba0
stephen.patterson <stephen.patterson@noaa.gov>
2024-09-20 15:26:52 -0400

    Finalized lidar pre-processing script

--- scripts/preprocess_lidar.py
+++ scripts/preprocess_lidar.py
@@ -1,4 +1,3 @@
-
 import pathlib
 import numpy as np
 import shutil
@@ -8,64 +7,125 @@ from osgeo import gdal, osr
 
 
 BASE_DIR = pathlib.Path(r'N:/HSD/Projects/HSD_DATA/NHSP_2_0/HH_2024/working/JABLTCX_data/Florida_wc/Florida_all_survey_years_raw')
-
 OUTPUT = pathlib.Path(r'N:\HSD\Projects\HSD_DATA\NHSP_2_0\HH_2024\working\sediment\dev')
 
 
-# get list of all rasters with rglob
-for i, raster in enumerate(BASE_DIR.rglob('*.tif')):
+def create_raster_vrt():
+    geotiffs = list(OUTPUT.rglob('*_5_5.tif'))
+    vrt_filename = str(OUTPUT / 'JABLTCX_mosaic.vrt')
+    gdal.BuildVRT(vrt_filename, geotiffs, callback=gdal.TermProgress_nocb)
+    final_image = gdal.Open(vrt_filename, gdal.GA_ReadOnly)
+    gdal.Translate(str(OUTPUT / 'JABLTCX_final.tif'), final_image, format='GTiff',
+               creationOptions=['COMPRESS:DEFLATE', 'TILED:YES'],
+               callback=gdal.TermProgress_nocb)
+    final_image = None
 
-    # 1. Copy out file
+
+def convert_band_ft_to_m(dataset):
     no_data = -999999
-    print(i, raster)
-    copy_raster = str(OUTPUT / raster.name)
-    shutil.copyfile(str(raster), copy_raster)
+    raster_array = dataset.ReadAsArray()
+    meters_array = np.where(raster_array == no_data, raster_array, raster_array * 0.3048006096)
+    dataset.GetRasterBand(1).WriteArray(meters_array)
 
-    # 2. Load dataset
-    raster_ds = gdal.Open(copy_raster, gdal.GA_Update)
-    projection = osr.SpatialReference(wkt=raster_ds.GetProjection())
+
+def get_epsq(dataset):
+    projection = osr.SpatialReference(wkt=dataset.GetProjection())
     projection.AutoIdentifyEPSG()
-    epsg = projection.GetAttrValue('AUTHORITY', 1)
-    units = projection.GetAttrValue('UNIT')
-    
-    # conversion
-    if epsg != '3747':
-        # 2. reproject
-        output_nad83_raster = str(OUTPUT / f'{raster.stem}_nad83.tif')
-        raster_warp = gdal.Warp(output_nad83_raster, raster_ds, dstSRS='EPSG:3747')
+    return projection.GetAttrValue('AUTHORITY', 1)
 
-        # reset raster_ds
-        raster_ds = None
-        raster_ds = raster_warp
-    if units != 'metre':
-        # 3. change units if project didn't do that already?!
-        raster_array = raster_ds.ReadAsArray()
-        meters_array = np.where(raster_array == no_data, raster_array, raster_array * 0.3048006096)
 
-        raster_ds.GetRasterBand(1).WriteArray(meters_array)
-    
-    _, cell_x, _, _, _, cell_y = raster_ds.GetGeoTransform()
-    if cell_x != 5.0 and cell_y != 5.0:
-        # 4. Resample to 5x5
-        output_nad83_raster = str(OUTPUT / f'{raster.stem}_nad83.tif')
-        raster_ds = gdal.Warp(output_nad83_raster, raster_ds, 
-                       format="GTiff",
-                       xRes=5.0, yRes=5.0,
-                       creationOptions=["COMPRESS=DEFLATE", "BIGTIFF=IF_NEEDED", "TILED=YES"])
-        _, cell_x, _, _, _, cell_y = raster_ds.GetGeoTransform()
-        print(cell_x, -cell_y)
-  
-    # 5. convert positive values to NaN
-    raster_array = raster_ds.ReadAsArray()
+def get_units(dataset):
+    projection = osr.SpatialReference(wkt=dataset.GetProjection())
+    return projection.GetAttrValue('UNIT')
+
+
+def get_raster_cell_size(dataset):
+    _, cell_x, _, _, _, cell_y = dataset.GetGeoTransform()
+    return cell_x, cell_y * -1  # inverse Y value
+
+
+def reproject_raster(dataset, raster_path):
+    output_nad83_raster = str(OUTPUT / f"{raster_path.stem}_nad83.tif")
+    gdal.Warp(
+        output_nad83_raster, 
+        dataset,
+        format="GTiff",
+        creationOptions=["COMPRESS=DEFLATE", "BIGTIFF=IF_NEEDED", "TILED=YES"],
+        dstSRS="EPSG:3747")
+
+    # reset raster_ds
+    dataset = None
+    raster_ds = gdal.Open(output_nad83_raster, gdal.GA_Update)
+    return raster_ds
+
+
+def resize_raster_grid(dataset, raster_path):
+    output_nad83_raster = str(OUTPUT / f'{raster_path.stem}_nad83_5_5.tif')
+    gdal.Warp(
+        output_nad83_raster,
+        dataset, # TODO are brackets needed? they got rid of error messages, but not Z aware now?
+        format="GTiff",
+        xRes=5.0,
+        yRes=5.0,
+        creationOptions=["COMPRESS=DEFLATE", "BIGTIFF=IF_NEEDED", "TILED=YES"],
+    )
+
+    # reset raster_ds
+    dataset = None
+    raster_ds = gdal.Open(output_nad83_raster, gdal.GA_Update)
+    return raster_ds
+
+
+def set_ground_to_nodata(dataset):
+    no_data = -999999
+    raster_array = dataset.ReadAsArray()
     meters_array = np.where(raster_array < 0, raster_array, no_data)
-    raster_ds.GetRasterBand(1).WriteArray(meters_array)
+    dataset.GetRasterBand(1).WriteArray(meters_array)
 
-    # check if units and res changed
-    raster_ds = None
 
-    if os.path.exists(str(OUTPUT / f'{raster.stem}_nad83.tif')):
-        os.remove(str(OUTPUT / raster.name))
-    if i == 3:
-        break
+def process():
+    # get list of all rasters with rglob
+    for i, raster_path in enumerate(BASE_DIR.rglob('*.tif')):
+        # Copy out file
+        print(i, raster_path)
+        copy_raster = str(OUTPUT / raster_path.name)
+        shutil.copyfile(str(raster_path), copy_raster)
 
-# 6. Build a VRT of all final raster files
+        # Load copied dataset
+        raster_ds = gdal.Open(copy_raster, gdal.GA_Update)
+        units = get_units(raster_ds)
+        epsg = get_epsq(raster_ds)
+
+        # This was failing when running it last
+        set_ground_to_nodata(raster_ds)
+        
+        if epsg != '3747':  # should we use Albers or some other projection?
+            raster_ds = reproject_raster(raster_ds, raster_path)
+
+        if units != 'metre':
+            convert_band_ft_to_m(raster_ds)
+        print('Output units:', get_units(raster_ds))
+        
+        cell_x, cell_y = get_raster_cell_size(raster_ds)
+        if cell_x != 5.0 and cell_y != 5.0:
+            raster_ds = resize_raster_grid(raster_ds, raster_path)
+            cell_x, cell_y = get_raster_cell_size(raster_ds)
+            print('Output cell size:', cell_x, -cell_y)
+
+        raster_ds = None
+
+        # Remove intermediate files
+        os.remove(str(OUTPUT / raster_path.name))
+        if os.path.exists(str(OUTPUT / f'{raster_path.stem}_nad83.tif')):
+            os.remove(str(OUTPUT / f'{raster_path.stem}_nad83.tif'))
+
+
+        if i == 4:
+            break
+
+    # 6. Build a VRT of all final raster files
+    create_raster_vrt()
+
+
+if __name__ == "__main__":
+    process()
