6a1fdd0e1bd5c48ff854b4b3daf2b5647f9afeb8
stephen.patterson <stephen.patterson@noaa.gov>
2025-03-18 16:24:25 -0400

    Filtering digital coast s3 bucket

--- src/hydro_health/engines/tiling/DigitalCoastProcessor.py
+++ src/hydro_health/engines/tiling/DigitalCoastProcessor.py
@@ -10,7 +10,6 @@ import pathlib
 import multiprocessing as mp
 import numpy as np
 
-from hydro_health.helpers import hibase_logging
 from botocore.client import Config
 from botocore import UNSIGNED
 
@@ -21,19 +20,57 @@ mp.set_executable(os.path.join(sys.exec_prefix, 'pythonw.exe'))
 class DigitalCoastProcessor:
     """Class for parallel processing all BlueTopo tiles for a region"""
 
-    def get_available_datasets(self, geometry_coords: str) -> None:
+    def download_bulk_data(self, download_link) -> None:
+        if 'noaa-nos-coastal-lidar-pds' in download_link:
+            _, data_file = download_link.replace('/index.html', '').split('.com')
+            lidar_bucket = self.get_bucket()
+            print('file:', data_file)
+            for obj_summary in lidar_bucket.objects.filter(Prefix=f"{data_file[1:]}"):
+                if 'tileindex' in obj_summary.key:
+                    print(obj_summary)
+                    # TODO each bucket folder has a tileindex zip file containing a shapefile, or a geopackage
+                    # 1. download shapefile or geopackage
+                    # 2. spatial query against drawn polygon
+                    # 3. loop through bucket again and download intersected tiles
+
+    def get_available_datasets(self, geometry_coords: str, outputs) -> None:
         """Query NOWCoast REST API for available datasets"""
 
-        base_url = 'https://maps.coast.noaa.gov/arcgis/rest/services/DAV/ElevationFootprints/MapServer/0/query?returnGeometry=false&f=json&where=1%3D1&outfields=%2A&spatialRel=esriSpatialRelIntersects&geometry='
+        base_url = 'https://maps.coast.noaa.gov/arcgis/rest/services/DAV/ElevationFootprints/MapServer/0/query?returnGeometry=false&f=json&where=1%3D1&outfields=%2A&spatialRel=esriSpatialRelIntersects&state=FL&geometry='
         elevation_footprints_url = base_url + geometry_coords
+        print(elevation_footprints_url)
         datasets_json = requests.get(elevation_footprints_url).json()
-        for feature in datasets_json['features']:
-            print(feature)
-            break
-            external_data_json = json.loads(feature['attributes']['ExternalProviderLink'])
-            for link in external_data_json['links']:
-                print(link['label'], link['link'])
 
+        output_json = pathlib.Path(outputs) / 'json_log.txt'
+        if os.path.exists(output_json):
+            output_json.unlink()
+        with open(output_json, 'a') as writer:
+            for feature in datasets_json['features']:
+                # TODO maybe store full JSON in a text in each data folder
+                # folder_name = f"{feature['provider_results_name']}_{feature['DataBin']}_{feature['Year']}"
+                # print(folder_name)
+                writer.write(json.dumps(feature, indent=4) + '\n\n')
+                external_data_json = json.loads(feature['attributes']['ExternalProviderLink'])
+                # Look for Bulk Download
+                for external_data in external_data_json['links']:
+                    if external_data['label'] == 'Bulk Download':
+                        download_link = external_data['link']
+                        self.download_bulk_data(download_link)
+                writer.write(json.dumps(external_data_json['links'], indent=4) + '\n\n')
+
+    def get_bucket(self):
+        """Connect to anonymous OCS S3 Bucket"""
+
+        bucket = "noaa-nos-coastal-lidar-pds"
+        creds = {
+            "aws_access_key_id": "",
+            "aws_secret_access_key": "",
+            "config": Config(signature_version=UNSIGNED),
+        }
+        s3 = boto3.resource('s3', **creds)
+        nbs_bucket = s3.Bucket(bucket)
+        return nbs_bucket
+    
     def get_geometry_string(self, tile_gdf: gpd.GeoDataFrame) -> str:
         """Build bbox string of tiles"""
 
@@ -44,14 +81,14 @@ class DigitalCoastProcessor:
         bbox = json.loads(tile_boundary.bounds.to_json())
         # lower-left to upper-right
         geometry_coords = f"{bbox['minx']['Polygon']},{bbox['miny']['Polygon']},{bbox['maxx']['Polygon']},{bbox['maxy']['Polygon']}"
-
+        print(geometry_coords)
         return geometry_coords
 
     def process(self, tile_gdf: gpd.GeoDataFrame, outputs: str = False):
         geometry_coords = self.get_geometry_string(tile_gdf)
 
         # 1. build URL and request JSON output
-        self.get_available_datasets(geometry_coords)
+        self.get_available_datasets(geometry_coords, outputs)
 
 
         # with self.get_pool() as process_pool:
