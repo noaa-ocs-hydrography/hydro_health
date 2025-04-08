304266b7bdd31e984351afb3521fdd2063e6cdfc
stephen.patterson <stephen.patterson@noaa.gov>
2025-04-08 11:05:45 -0400

    Added ecoregion id to sample file output

--- scripts/create_ecoregion_masks.py
+++ scripts/create_ecoregion_masks.py
@@ -1,5 +1,6 @@
 import pathlib
 import datetime
+import json
 from osgeo import gdal, ogr, osr
 gdal.SetConfigOption('CHECK_DISK_FREE_SPACE', 'FALSE')
 
@@ -7,7 +8,7 @@ INPUTS = pathlib.Path(__file__).parents[1] / 'inputs'
 OUTPUTS = pathlib.Path(__file__).parents[1] / 'outputs'
 
 
-def get_eco_region_polygons():
+def build_prediction_masks():
     gpkg = INPUTS / 'Master_Grids.gpkg'
     gpkg_ds = ogr.Open(gpkg)
     ecoregions_50m = gpkg_ds.GetLayerByName('EcoRegions_50m')
@@ -19,15 +20,16 @@ def get_eco_region_polygons():
 
     ecoregions_50m.ResetReading()
     in_memory = ogr.GetDriverByName('Memory')
-    for index, feature in enumerate(ecoregions_50m):
-        i = index + 1
-        in_memory_ds = in_memory.CreateDataSource(str(OUTPUTS / f'output_layer_{i}.shp'))
-        in_memory_layer = in_memory_ds.CreateLayer(f'poly_{i}', srs=ecoregions_50m.GetSpatialRef(), geom_type=ogr.wkbPolygon)
+    for feature in ecoregions_50m:
+        feature_json = json.loads(feature.ExportToJson())
+        ecoregion_id = feature_json['properties']['EcoRegion']
+        in_memory_ds = in_memory.CreateDataSource(str(OUTPUTS / f'output_layer_{ecoregion_id}.shp'))
+        in_memory_layer = in_memory_ds.CreateLayer(f'poly_{ecoregion_id}', srs=ecoregions_50m.GetSpatialRef(), geom_type=ogr.wkbPolygon)
         mem_poly = ogr.Feature(in_memory_layer.GetLayerDefn())
         mem_poly.SetGeometry(feature.GetGeometryRef())
         in_memory_layer.CreateFeature(mem_poly)
 
-        mask_path = OUTPUTS / f'ecoregions_50m_mask_{i}.tif'
+        mask_path = OUTPUTS / f'ecoregions_50m_mask_{ecoregion_id}.tif'
         with gdal.GetDriverByName("GTiff").Create(
             str(mask_path),
             x_res,
@@ -48,12 +50,13 @@ def get_eco_region_polygons():
         
 
         in_memory_layer = None
+        break
 
 
 def process():
     start = datetime.datetime.now()
     print('Starting')
-    get_eco_region_polygons()
+    build_prediction_masks()
     print(f'Done: {datetime.datetime.now() - start}')
 
 if __name__ == '__main__':
