bfa5f9be7568ed2cac95de62fbadc9bf3334e5c9
stephen.patterson <stephen.patterson@noaa.gov>
2025-12-29 11:32:46 -0500

    Added script to upload files to AGO

--- scripts/upload_tifs_to_ago.py
+++ scripts/upload_tifs_to_ago.py
@@ -1,5 +1,8 @@
 import os
+import pathlib
 from arcgis.gis import GIS
+from arcgis.raster import utils
+
 
 def upload_tif_files_via_pro(folder_path, folder_name):
     print("Connecting via ArcGIS Pro active session...")
@@ -13,24 +16,43 @@ def upload_tif_files_via_pro(folder_path, folder_name):
 
     for file_name in os.listdir(folder_path):
         if file_name.lower().endswith(('.tif', '.tiff')):
-            file_path = os.path.join(folder_path, file_name)   
+            file_path = pathlib.Path(folder_path) / file_name   
             item_properties = {
                 "title": os.path.splitext(file_name)[0],
-                "type": "Image",
+                "productType": "Raster Dataset",
                 "tags": "HydroHealth",
                 "description": f"{file_name}"
             }
             
             print(f"Uploading {file_name}...")
             try:
-                upload_folder = gis.content.folders.get(folder_name)
-                uploaded_item = upload_folder.add(item_properties=item_properties, data=file_path)
-                print(f"Successfully uploaded: {uploaded_item.title} (ID: {uploaded_item.id})")
+
+                properties = {
+                    'title': file_path.stem,
+                    'description': f'Hydro Health 2025 - Ecoregion 3 - {file_name}',
+                    'tags': ['HydroHealth'],
+                    'snippet': file_name
+                }
+                imagery_layer_item = utils.publish_hosted_imagery_layer(
+                    input_data=str(file_path),
+                    gis=gis,
+                    layer_configuration="ONE_IMAGE",
+                    tiles_only=False,
+                    raster_type_name='Raster Dataset',
+                    output_name=file_path.stem,
+                    folder=folder_name
+                )
+                print(f"Successfully uploaded: {imagery_layer_item.title} (ID: {imagery_layer_item.id})")
+
+                hosted_layer_item = gis.content.get(imagery_layer_item.id)
+                hosted_layer_item.update(item_properties=properties)
+                print(f"Successfully updated item properties")
+                
             except Exception as e:
                 print(f"Failed to upload {file_name}. Error: {e}")
 
 
-LOCAL_FOLDER = r"input/folder/of/tif/files"
+LOCAL_FOLDER = r"N:\CSDL\Projects\Hydro_Health_Model\HHM2025\working\HHM_Run\ER_3\ER3_NOAA_AGO_Files\Pilot_Model_Files\Input_data\problem_files"
 FOLDER_NAME = "ER3_Input_Data"
 
 
