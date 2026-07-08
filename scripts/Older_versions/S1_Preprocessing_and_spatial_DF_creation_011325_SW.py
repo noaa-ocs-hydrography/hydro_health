4ecee915d3b50ff7f1ef8dfd8094067b46e232f8
stephen.patterson <stephen.patterson@noaa.gov>
2026-07-08 16:57:14 -0700

    Created a branch to host Pages for provider checker

--- scripts/Older_versions/S1_Preprocessing_and_spatial_DF_creation_011325_SW.py
+++
@@ -1,6 +0,0 @@
-# Script to be translated over from R version 
-# Stephen has already done this for part of the code origonally, code needs amalgamated and superfluous script deleted
-
-# need to consider if we want to update to python as is, or move forard straight into the new version which 
-# brings in the data in raw tile format as it is from now coast. This will also mean that other predictors i.e hurricanes, TSM,
-# sediment and gravel will need to be split out over the tiles / sub grid 
